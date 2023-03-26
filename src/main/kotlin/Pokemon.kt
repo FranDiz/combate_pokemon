c1f75869684d711f6b41e51718028067cbbfa4b3
Fran <Fran_Diz@outlook.com>
2023-03-26 14:23:17 +0200

    clase pokemon

---
+++ src/main/kotlin/Pokemon.kt
@@ -0,0 +1,7 @@
+class Pokemon (nombre:String, lore:String, tipo:Tipo, fuerza:Int, vida:Int){
+    var nombre=nombre
+    var lore=lore
+    var tipo=tipo
+    var fuerza=fuerza
+    var vida=vida
+}
