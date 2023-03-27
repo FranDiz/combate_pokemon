71dd09c637670ad36555254a866afa8370637e7d
Fran <Fran_Diz@outlook.com>
2023-03-27 17:09:22 +0200

    Clases pokemon y jugador

--- src/main/kotlin/Jugador.kt
+++ src/main/kotlin/Jugador.kt
@@ -1,9 +1,11 @@
 class Jugador (pokemon: Pokemon) {
-    init{
-        val Charizard = Pokemon("Charmander", "Salamandra llameante", Tipo("fuego"), 50, 300)
-        val Blastoise = Pokemon("Blastoise", "Tortuga bombardera", Tipo("agua"), 45, 350)
-        val Agumon = Pokemon("Agumon", "Reptiliano trotamundos", Tipo("tierra"), 60, 300)
-        val Bulbasaur = Pokemon("Bulbasaur", "Planta infecciosa", Tipo("planta"), 30, 200)
-        var lista_pokemons = arrayOf<Pokemon>(Charizard, Blastoise, Agumon, Bulbasaur)
+    val Charizard = Pokemon("Charmander", "Salamandra llameante", Tipo("fuego"),  300)
+    val Blastoise = Pokemon("Blastoise", "Tortuga bombardera", Tipo("agua"), 350)
+    val Agumon = Pokemon("Agumon", "Reptiliano trotamundos", Tipo("tierra"), 300)
+    val Bulbasaur = Pokemon("Bulbasaur", "Planta infecciosa", Tipo("planta"), 200)
+    var lista_pokemons = arrayOf<Pokemon>(Charizard, Blastoise, Agumon, Bulbasaur)
+    fun elegirPokemon(){
+        println("Elige tu pokemon: ${Charizard.nombre}, ${Blastoise.nombre}, ${Agumon.nombre}, ${Bulbasaur.nombre}")
+
     }
 }
--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -1,11 +1,10 @@
-class Pokemon (nombre:String, lore:String, tipo:Tipo, fuerza:Int, vida:Int){
+class Pokemon (nombre:String, lore:String, tipo:Tipo, vida:Int){
     var nombre=nombre
     var lore=lore
     var tipo=tipo
-    var fuerza=fuerza
     var vida=vida
 
     fun recibirAtaque(){
-        this.vida-this.fuerza
+        this.vida-
     }
 }
