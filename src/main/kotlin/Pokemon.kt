ef7822181e3c72ff4cd72207cb348bf97cc90910
Fran <Fran_Diz@outlook.com>
2023-03-27 16:49:28 +0200

    Pokemon funcion

--- src/main/kotlin/Pokemon.kt
+++ src/main/kotlin/Pokemon.kt
@@ -4,4 +4,8 @@ class Pokemon (nombre:String, lore:String, tipo:Tipo, fuerza:Int, vida:Int){
     var tipo=tipo
     var fuerza=fuerza
     var vida=vida
+
+    fun recibirAtaque(){
+        this.vida-this.fuerza
+    }
 }
